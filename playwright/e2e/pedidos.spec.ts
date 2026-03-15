7c1c3e2c5e7c7f6fce28daaa78551dd121166dfa
Alan Rodrigues <alanrsilverti@gmail.com>
2026-03-15 16:03:05 -0300

    iniciando automação do teste de consulta de pedido aprovado - ajustando tovisible

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -7,13 +7,17 @@ test('deve consulta um pedido aprovado', async ({ page }) => {
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-//Checkpoint
+
+
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   await page.getByTestId('search-order-id').fill('VLO-O73Z0R')
+
   await page.getByTestId('search-order-button').click()
 
-
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-O73Z0R')
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
