aa6303ae029b6bb8917916b94840d0b2ae78b386
Alan Rodrigues <alanrsilverti@gmail.com>
2026-03-15 16:10:42 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,23 +1,26 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consulta um pedido aprovado', async ({ page }) => {
 
+ //Arrange
   await page.goto('http://localhost:5173/')
-
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-O73Z0R')
 
+  //Act
   await page.getByTestId('search-order-button').click()
 
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-O73Z0R')
 
+  //Assert
   await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
