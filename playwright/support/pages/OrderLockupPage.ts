42d2493ccee07528638e713e432b9a29487d6067
alanrsilverti-ctrl <alanrsilverti@gmail.com>
2026-06-04 22:37:21 -0300

    PageObject implementado na mão

--- playwright/support/pages/OrderLockupPage.ts
+++ playwright/support/pages/OrderLockupPage.ts
@@ -15,26 +15,70 @@ export class OrderLockupPage {
 
         const statusClasses = {
             APROVADO: {
-                background: 'bg-green-100', 
-                text: 'text-green-700', 
-                icon: 'lucide-circle-check-big'},
-            
+                background: 'bg-green-100',
+                text: 'text-green-700',
+                icon: 'lucide-circle-check-big'
+            },
+
             REPROVADO: {
-                background: 'bg-red-100', 
-                text: 'text-red-700', 
-                icon: 'lucide-circle-x'},
-            
+                background: 'bg-red-100',
+                text: 'text-red-700',
+                icon: 'lucide-circle-x'
+            },
+
             EM_ANALISE: {
-                background: 'bg-amber-100', 
-                text: 'text-amber-700', 
-                icon: 'lucide-clock'},
+                background: 'bg-amber-100',
+                text: 'text-amber-700',
+                icon: 'lucide-clock'
+            },
         } as const
 
-    const classes = statusClasses[status]
-    const statusBadge = this.page.getByRole('status').filter({ hasText: status })
+        const classes = statusClasses[status]
+        const statusBadge = this.page.getByRole('status').filter({ hasText: status })
 
-    await expect(statusBadge).toHaveClass(new RegExp(classes?.background))
-    await expect(statusBadge).toHaveClass(new RegExp(classes?.text))
-    await expect(statusBadge.locator('svg')).toHaveClass(new RegExp(classes?.icon))
+        await expect(statusBadge).toHaveClass(new RegExp(classes?.background))
+        await expect(statusBadge).toHaveClass(new RegExp(classes?.text))
+        await expect(statusBadge.locator('svg')).toHaveClass(new RegExp(classes?.icon))
+    }
+
+    async validateOrderDetails(order: any) {
+        await expect(this.page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: ${order.number}
+            - status:
+              - img
+              - text: ${order.status}
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: ${order.color}
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: ${order.wheels}
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: ${order.customer.name}
+            - paragraph: Email
+            - paragraph: ${order.customer.email}
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: ${order.payment}
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
+
+    }
+
+    async validateOrderNotFound() {
+        await expect(this.page.locator('#root')).toMatchAriaSnapshot(`
+        - img
+        - heading "Pedido não encontrado" [level=3]
+        - paragraph: Verifique o número do pedido e tente novamente
+        `)
     }
 }
